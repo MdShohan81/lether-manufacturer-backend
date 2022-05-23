adfd0a69edae505768064602dd1172da818985ec
MdShohan81 <mdshohan6563@gmail.com>
2022-05-23 12:14:13 +0600

    added product's dynamic api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,13 @@ async function run(){
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const products = await productCollection.findOne(query);
             res.send(products);
         })
         
