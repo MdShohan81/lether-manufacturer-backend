65e461fc6bd87d64ee717f6942cc5c3719601790
MdShohan81 <mdshohan6563@gmail.com>
2022-05-24 23:05:33 +0600

    get order data and call api

--- index.js
+++ index.js
@@ -37,10 +37,21 @@ async function run(){
             res.send(products);
         });
 
+        //order post api
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result)
+        });
+
+        // order get api
+
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
         })
 
         
