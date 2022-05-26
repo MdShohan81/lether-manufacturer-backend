5a4c78a3749f68c7f757345dabda4d37de38f2dc
MdShohan81 <mdshohan6563@gmail.com>
2022-05-26 19:36:01 +0600

    added admin api

--- index.js
+++ index.js
@@ -59,6 +59,25 @@ async function run(){
             res.send(products);
         });
 
+        //get user
+        app.get('/user', verifyJWT, async(req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
+        });
+
+        //admin api
+        app.put('/user/:admin/:email', async(req, res) => {
+            const email = req.params.email;
+            
+            const filter = {email: email};
+            
+            const updateDoc = {
+                $set: {role: 'admin'},
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
         //users api
         app.put('/user/:email', async(req, res) => {
             const email = req.params.email;
